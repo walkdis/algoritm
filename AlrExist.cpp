ba46421a2319ba2d7a6611f3e46cf5b856a6ebf3
walkdis <walkdis.as@gmail.com>
2016-10-31 21:16:49 +0300

    Update AlrExist.cpp

--- AlrExist.cpp
+++ AlrExist.cpp
@@ -1,1 +1,14 @@
+// Copyright 2016 <Anna Simakova>
+#include "AlrExist.h"
+#include <string>
 
+
+AnimalAlrExist::AnimalAlrExist(double x, double y) :exception() {
+    string textx, texty;
+    textx = x;
+    texty = y;
+    msg = "Animal with coordinates: " + textx + texty + " already exist";
+}
+const char * AnimalAlrExist::what() const throw() {
+    return msg.c_str();
+}

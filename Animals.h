0d0e187d8677b1b20d2f0eef88e8d90e9cdef1a3
walkdis <walkdis.as@gmail.com>
2016-10-31 20:39:54 +0300

    Update and rename .gitignore to Animals.h

---
+++ Animals.h
@@ -0,0 +1,36 @@
+// Copyright 2016 <Anna Simakova>
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+
+
+using ::std::cin;
+using ::std::cout;
+using ::std::ostream;
+using std::endl;
+
+class Animals {
+    double x;
+    double y;
+    char type;
+    double p;
+
+
+public:
+    Animals(double x, double y, char type);
+    Animals(double x, double y, char type, double p);
+    ~Animals();
+    friend ostream& operator<<(ostream& stream, const Animals& anim);
+    double GetX();
+    double GetY();
+    char GetType();
+    double GetP();
+    double SetX(double x);
+    double SetY(double y);
+    char SetType(char type);
+    double SetP(double p);
+    Animals(const Animals& anim);
+    const Animals& operator =(const Animals& anim);
+    bool operator ==(const Animals& anim) const;
+};

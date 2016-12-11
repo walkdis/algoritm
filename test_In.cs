670671b005416d28119b0ba41976f0bafcbc87c3
walkdis <walkdis.as@gmail.com>
2016-12-11 14:40:51 +0300

    Update test_In.cs

--- test_In.cs
+++ test_In.cs
@@ -1,24 +1,86 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System; 
 using System.IO; 
 
-namespace test_In
-{
-    class Program
-    {
-        static void Main(string[] args){
-                Console.Write("Ведите адрес входного файла и нажмите Enter.\n");
-                Console.Write("пример ");
-                Console.Write(@"C:\Users\WalkDis\dz\in.txt");
-                Console.Write("\n");
-                string adres1;
-                adres1 = Console.ReadLine();
-                Console.Write("я не знаю, что тестировать на ввод или вывод. меня в принципе все устраивает в моей программе. ");
-                Console.Write("К тому же там есть исключения. а писать тест, состоящий из сравнения уже готового решения с выданным я не хочу т.к. это глупо.");
-                Console.ReadLine();
-        }
-    }
+namespace Test_in_out 
+{ 
+class Program 
+{ 
+static void Main(string[] args) 
+{ 
+string adres1; 
+int N = 0, M = 0, Start1 = 0, ver = 0, zifra = 0,rowCount,colCount; 
+string[] lines; 
+string[] simbol; 
+int[] array; 
+try 
+
+{ 
+Console.Write("Ведите адрес входного файла и нажмите Enter.\n"); 
+Console.Write(@"C:\Users\WalkDis\Desktop\qwe.txt"); 
+Console.Write("\n"); 
+
+adres1 = Console.ReadLine(); 
+
+lines = File.ReadAllLines(@adres1); 
+rowCount = lines.Length; 
+colCount = lines[0].Split().Length; 
+
+simbol = lines[0].Split(); 
+zifra=lines[0].Length; 
+array = new int[zifra]; 
+
+Console.Write("\n\n"); 
+
+try 
+{ 
+for (int i = 0; i < zifra; i++) 
+{ 
+array[i] = int.Parse(simbol[i]); 
+if (simbol[i] != " ") { Start1++; } 
+} 
+} 
+catch { } 
+
+for (int i = 0; i < Start1; i++) 
+{ 
+N++; 
+if (array[i] < 0) { M = 1; } 
+array[i] = int.Parse(simbol[i]); 
+if (N == 4) 
+{ 
+if (array[i] < 101 && array[i] > 0) {} 
+else { ver = 1; } 
+} 
+Console.Write(array[i]); 
+Console.Write(" "); 
+
+} 
+Console.Write("\n"); 
+Console.Write("ОК Всего элементов:"); 
+
+
+Console.Write(Start1); 
+Console.Write("\n"); 
+if (ver == 0) { Console.Write("ОК Вероятность от 1 до 100:"); } 
+else { Console.Write("Error Вероятность указана с ошибкой:"); } 
+Console.Write("\n"); 
+if (Start1 % 4 == 0) 
+Console.WriteLine("OK Количество элементов кратно 4"); 
+else 
+Console.WriteLine("OK Количество элементов не кратно 4"); 
+
+if (M == 1) { Console.Write("ERROR Есть отрицательные элементы"); } 
+else { Console.Write("OK Все элементы положительные"); } 
+} 
+catch 
+{ 
+Console.Write("\n"); 
+Console.Write("Error.\n"); 
+
+Console.Write("\n\n"); 
+} 
+
+Console.ReadKey(); 
+} 
+} 
 }
